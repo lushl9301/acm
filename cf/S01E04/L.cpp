352707ff91c8da88c16ec8a91d20fb4ef578f917
Zhou Xinzi <imdreamrunner@gmail.com>
2013-10-03 12:04:36 +0800

    Change L to another WA version

--- cf/S01E04/L.cpp
+++ cf/S01E04/L.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 #include "stdio.h"
 
-char NO_CIRCLE = 128;
 char NO_CHOICE = 128;
 
 int size;
-char dict[128], theCount[128], theCircle[128], circle[27], choice[128];
+char dict[128], theCount[128], theCircle[128], circle[27], choice[128], stri[1000001], strd[128];
 char theChar[128][27];
 
 int main() {
     int test;
     scanf("%d", &test);
-    char stri[1000001], strd[128];
     while (test-- > 0) {
-        for (int i = 0; i < 27; i++) {
-            circle[i] = NO_CIRCLE;
-        }
         scanf("%d", &size);
         scanf("%s", stri);
         scanf("%s", strd);
@@ -33,6 +28,7 @@ int main() {
                 //printf(" = >%d  %d\n", current, i);
                 if (theCircle[i] == 0 && current == i) {
                     theCircle[i] = k;
+                    break;
                 }
                 theChar[i][k] = current;
                 if (current < min) {
@@ -45,6 +41,9 @@ int main() {
         
         char nowChar, nowCircle, useCount = 0, testCircle;
         bool found;
+        for (char i = 0; i < 27; i++) {
+            circle[i] = NO_CHOICE;
+        }
         for (int i = 0; i < size; i ++ ) {
             if (choice[stri[i]] != NO_CHOICE) {
                 printf("%c",choice[stri[i]]);
@@ -53,7 +52,7 @@ int main() {
             nowChar = stri[i];
             nowCircle = theCircle[nowChar];
             //printf(" nowCircle %d -> %d\n",nowChar, nowCircle );
-            if (circle[nowCircle] == NO_CIRCLE) {
+            if (circle[nowCircle] == NO_CHOICE) {
                 useCount = theCount[nowChar];
                 found = false;
                 testCircle = nowCircle;
@@ -63,34 +62,34 @@ int main() {
                 }
                 if (!found) {
                     while (testCircle <= 26) { // 2 should follow 4's Circle
-                        if (circle[testCircle] != NO_CIRCLE) {
-                            useCount = circle[testCircle];
+                        if (circle[testCircle] != NO_CHOICE) {
+                            useCount = circle[testCircle] % nowCircle;
                             found = true;
                         }
                         testCircle *= 2;
-                    }                }
-                if (!found) {
-                    if (nowCircle > 2) {
-                        for (testCircle = (char)(nowCircle - 1); testCircle > 1; testCircle--) {
-                            // for 4 , use some 2's Circle
-                            if (nowCircle % testCircle == 0) {
-                                if (circle[testCircle] != NO_CIRCLE) {
-                                    char testCount = useCount = circle[testCircle];
-                                    char smallestChar = theChar[nowChar][testCount];
-                                    testCount += testCircle;
-                                    while (testCount <= 26) {
-                                        if (theChar[nowChar][testCount] < smallestChar) {
-                                            useCount = testCount;
-                                            smallestChar = theChar[nowChar][testCount];
-                                        }
-                                        testCount += testCircle;
-                                    }
-                                    break;
-                                }
+                    }
+                }
+                if (!found && nowCircle > 2) {
+                    bool possibleCount[27];
+                    for (char j = 0; j < 27; j ++) possibleCount[j] = false;
+                    for (testCircle = (char)(nowCircle - 1); testCircle > 1; testCircle--) {
+                        // for 4 , use some 2's Circle
+                        if (nowCircle % testCircle == 0 && circle[testCircle] != NO_CHOICE) {
+                            char pc = circle[testCircle];
+                            while (pc <= nowCircle) {
+                                possibleCount[pc] = true;
+                                pc += testCircle;
                             }
                         }
-                        
                     }
+                    char minChar = 'z' + 1;
+                    for (char j = nowCircle; j > 0; j --) {
+                        if (possibleCount[j] && theChar[nowChar][j] < minChar) {
+                            minChar = theChar[nowChar][j];
+                            useCount = j;
+                        }
+                    }
+                    
                 }
                 circle[nowCircle] = useCount;
             }
