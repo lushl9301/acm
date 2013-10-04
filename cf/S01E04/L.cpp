b0b208dd77a3f27dcbcf209c4b67c261ab74c487
Zhou Xinzi <imdreamrunner@gmail.com>
2013-10-04 16:00:52 +0800

    Add the AC version of q L in S01E04

--- cf/S01E04/L.cpp
+++ cf/S01E04/L.cpp
@@ -1,36 +1,48 @@
+#include <cstdio>
 #include <iostream>
-#include "stdio.h"
+
+using namespace std;
+
 
 char NO_CHOICE = 128;
+int NO_CIRCLE = 128;
 
-int size;
-char dict[128], theCount[128], theCircle[128], circle[27], choice[128], stri[1000001], strd[128];
-char theChar[128][27];
+int test, size;
+char stri[1000001], strd[128];
+int dict[26], choice[26], theCount[26], theCircle[26], circle[27];
+char theChar[26][27];
+
+int getGCD(int u, int v) {
+    while ( v != 0) {
+        int r = u % v;
+        u = v;
+        v = r;
+    }
+    return u;
+}
 
 int main() {
-    int test;
     scanf("%d", &test);
     while (test-- > 0) {
         scanf("%d", &size);
         scanf("%s", stri);
         scanf("%s", strd);
-        for (char i = 0; i < 26; i ++) {
-            dict[strd[i]] = i + 'a';
+        for (int i = 0; i < 26; i ++) {
+            dict[strd[i] - 'a'] = i;
         }
-        for (char i = 'a'; i <= 'z'; i ++) {
+        for (int i = 0; i < 26; i ++) {
             theCircle[i] = 0;
             choice[i] = NO_CHOICE;
-            char min = i, current=i;
-            char num = 0;
+            char min = i;
+            int current=i, num = 0;
             theChar[i][0] = current;
-            for (char k = 1; k <= 26; k++) {
+            for (int k = 1; k <= 26; k++) {
                 current = dict[current];
-                //printf(" = >%d  %d\n", current, i);
+                theChar[i][k] = current;
                 if (theCircle[i] == 0 && current == i) {
                     theCircle[i] = k;
                     break;
                 }
-                theChar[i][k] = current;
                 if (current < min) {
                     min = current;
                     num = k;
@@ -38,63 +50,47 @@ int main() {
             }
             theCount[i] = num;
         }
-        
-        char nowChar, nowCircle, useCount = 0, testCircle;
-        bool found;
-        for (char i = 0; i < 27; i++) {
-            circle[i] = NO_CHOICE;
+
+        int nowChar, nowCircle, useCount = 0, testCircle;
+        for (int i = 0; i < 27; i++) {
+            circle[i] = NO_CIRCLE;
         }
         for (int i = 0; i < size; i ++ ) {
-            if (choice[stri[i]] != NO_CHOICE) {
-                printf("%c",choice[stri[i]]);
+            nowChar = stri[i] - 'a';
+            if (choice[nowChar] != NO_CHOICE) {
+                printf("%c", choice[nowChar] + 'a');
                 continue;
             }
-            nowChar = stri[i];
             nowCircle = theCircle[nowChar];
-            //printf(" nowCircle %d -> %d\n",nowChar, nowCircle );
-            if (circle[nowCircle] == NO_CHOICE) {
+            if (circle[nowCircle] == NO_CIRCLE) {
                 useCount = theCount[nowChar];
-                found = false;
                 testCircle = nowCircle;
-                testCircle *= 2;
-                if (testCircle == 1) {
-                    found = true;
-                }
-                if (!found) {
-                    while (testCircle <= 26) { // 2 should follow 4's Circle
-                        if (circle[testCircle] != NO_CHOICE) {
-                            useCount = circle[testCircle] % nowCircle;
-                            found = true;
-                        }
-                        testCircle *= 2;
-                    }
-                }
-                if (!found && nowCircle > 2) {
-                    bool possibleCount[27];
-                    for (char j = 0; j < 27; j ++) possibleCount[j] = false;
-                    for (testCircle = (char)(nowCircle - 1); testCircle > 1; testCircle--) {
-                        // for 4 , use some 2's Circle
-                        if (nowCircle % testCircle == 0 && circle[testCircle] != NO_CHOICE) {
-                            char pc = circle[testCircle];
-                            while (pc <= nowCircle) {
-                                possibleCount[pc] = true;
-                                pc += testCircle;
+                bool possibleCount[27];
+                for (int j = 0; j < 27; j ++) possibleCount[j] = true;
+                for (testCircle = 0; testCircle < 27; testCircle++) {
+                    int gcd = getGCD(nowCircle, testCircle);
+                    if (gcd > 1 && circle[testCircle] != NO_CIRCLE) {
+                        int pc = 0;
+                        while (pc <= nowCircle) {
+                            if (pc % gcd != circle[testCircle] % gcd) {
+                                possibleCount[pc] = false;
                             }
+                            pc += 1;
                         }
                     }
-                    char minChar = 'z' + 1;
-                    for (char j = nowCircle; j > 0; j --) {
-                        if (possibleCount[j] && theChar[nowChar][j] < minChar) {
-                            minChar = theChar[nowChar][j];
-                            useCount = j;
-                        }
+                }
+                int minChar = 27;
+                for (int j = 0; j < nowCircle; j ++) {
+                    if (possibleCount[j] && theChar[nowChar][j] < minChar) {
+                        minChar = theChar[nowChar][j];
+                        useCount = j;
                     }
-                    
                 }
                 circle[nowCircle] = useCount;
             }
-            choice[stri[i]] = theChar[nowChar][circle[nowCircle]];
-            printf("%c",choice[stri[i]]);
+            choice[nowChar] = theChar[nowChar][circle[nowCircle]];
+
+            printf("%c",choice[nowChar] + 'a');
         }
         printf("\n");
     }
